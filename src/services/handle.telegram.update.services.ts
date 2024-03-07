5b0877f6a5e9da63a9210a26f7eb979e389bdaf0
Alexandr2105 <5030553@gmail.com>
2024-03-07 11:34:11 +0300

    test vercel

--- src/services/handle.telegram.update.services.ts
+++ src/services/handle.telegram.update.services.ts
@@ -5,29 +5,33 @@ export class HandleTelegramUpdateServices {
     private supportTelegramServices = new SupportTelegramServices();
 
     async sendMessage(data: TelegramMessageType) {
-        const chatId = data.message.chat.id;
-        const message = data.message.text;
+        if (data?.message) {
+            const chatId = data.message.chat.id;
+            const message = data.message.text;
 
-        switch (message) {
-            case "/start":
-                await this.supportTelegramServices.startService(data.message.from.first_name, chatId);
-                break;
-            case "Привет":
-                await this.supportTelegramServices.privetService(chatId);
-                break;
-            case "Выбрать тариф":
-                await this.supportTelegramServices.selectPlansService(chatId);
-                break;
-            case "Базовый":
-            case "Продвинутый":
-            case "Pro":
-                await this.supportTelegramServices.plansKeyboardService(chatId);
-                break;
-            case "Попробовать бесплатно":
-                await this.supportTelegramServices.selectFreePlanService(chatId);
-                break;
-            default:
-                await this.supportTelegramServices.defaultService(chatId);
+            switch (message) {
+                case "/start":
+                    await this.supportTelegramServices.startService(data.message.from.first_name, chatId);
+                    break;
+                case "Привет":
+                    await this.supportTelegramServices.privetService(chatId);
+                    break;
+                case "Выбрать тариф":
+                    await this.supportTelegramServices.selectPlansService(chatId);
+                    break;
+                case "Базовый":
+                case "Продвинутый":
+                case "Pro":
+                    await this.supportTelegramServices.plansKeyboardService(chatId);
+                    break;
+                case "Попробовать бесплатно":
+                    await this.supportTelegramServices.selectFreePlanService(chatId);
+                    break;
+                default:
+                    await this.supportTelegramServices.defaultService(chatId);
+            }
+        }else {
+            console.log("Error")
         }
     }
 }
