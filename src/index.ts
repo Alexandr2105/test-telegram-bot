9960462e000c2b0343c34ad9f02de3fe8f047d59
Alexandr2105 <5030553@gmail.com>
2024-03-07 11:19:49 +0300

    test bot on vercel and delete picture folder

--- src/index.ts
+++ src/index.ts
@@ -18,15 +18,15 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
 
-// async function start() {
-//     let baseUrl = settings.CURRENT_APP_BASE_URL;
-//     const telegramAdapter = new TelegramAdapter();
-//     if (!baseUrl) {
-//         baseUrl = await telegramAdapter.connectToNgrok();
-//     }
-//     await telegramAdapter.sendOurHookForTelegram(
-//         baseUrl + '/telegram/webhook',
-//     );
-// }
-//
-// start();
+async function start() {
+    let baseUrl = settings.CURRENT_APP_BASE_URL;
+    const telegramAdapter = new TelegramAdapter();
+    if (!baseUrl) {
+        baseUrl = await telegramAdapter.connectToNgrok();
+    }
+    await telegramAdapter.sendOurHookForTelegram(
+        baseUrl + '/telegram/webhook',
+    );
+}
+
+start();
